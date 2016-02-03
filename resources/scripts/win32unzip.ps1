183fb6c6b64359e653ad49e15aff068d2e281a03
Nick Logan <ugexe@cpan.org>
2016-02-02 19:27:26 -0500

    win32unzip.ps1 can list file contents

--- resources/scripts/win32unzip.ps1
+++ resources/scripts/win32unzip.ps1
@@ -5,19 +5,25 @@ param (
 $shell = New-Object -com shell.application
 $zip = $shell.NameSpace($file)
 
+function List-ZipFiles {
+    $ns = $shell.NameSpace($args[0])
+    foreach( $item in $ns.Items() ) {
+        if( $item.IsFolder ) {
+            Get-ZipFiles($item)
+        } else {
+            $path = $item | Select -ExpandProperty Path
+            Write-Host($path)
+        }
+    }
+}
+
 if( $out -ne '' ) {
     $items = $zip.items()
     $to = $shell.NameSpace($out)
     $to.CopyHere($items, 0x14)
 } else {
-    $path = $zip.items() | Select -ExpandProperty Path
+    $items = $zip.items()
+    $path = $items | Select -ExpandProperty Path
     Write-Host $path
-
-    # TODO:
-    # Make this list *all files* in an archive. Currently only lists
-    # the root directory of the archive. However the root can be used
-    # to get the files/folders inside, which could then be filtered by
-    # file/folder while recursively repeating this on folders
-    # $root = $shell.NameSpace($path)
-    # $rootItems = $root.Items() | Select -ExpandProperty Path
+    List-ZipFiles $path
 }

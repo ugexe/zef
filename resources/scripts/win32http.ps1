5541ca3ce9b17600934379ffd98ec0a4190860b7
Nick Logan <nlogan@gmail.com>
2017-09-16 21:19:35 -0400

    Fix powershell download script

--- resources/scripts/win32http.ps1
+++ resources/scripts/win32http.ps1
@@ -1,18 +1,22 @@
 Param (
     [Parameter(Mandatory=$True)] [System.Uri]$uri,
-    [Parameter(Mandatory=$True )] [string]$FilePath
+    [Parameter(Mandatory=$True)] [string]$FilePath,
+    $UserAgent = "rakudo perl6/zef powershell downloader"
 )
 
 if ( -not (Test-Path $FilePath) ) {
-    [void] (New-Object System.Net.WebClient).DownloadFile($uri.ToString(), $FilePath)
+    $client = New-Object System.Net.WebClient;
+    $client.Headers['User-Agent'] = $UserAgent;
+    $client.DownloadFile($uri.ToString(), $FilePath)
 } else {
     try {
         $webRequest = [System.Net.HttpWebRequest]::Create($uri);
         $webRequest.IfModifiedSince = ([System.IO.FileInfo]$FilePath).LastWriteTime
+        $webRequest.UserAgent = $UserAgent;
         $webRequest.Method = "GET";
         [System.Net.HttpWebResponse]$webResponse = $webRequest.GetResponse()
 
-        $stream = New-Object System.IO.StreamReader($response.GetResponseStream())
+        $stream = New-Object System.IO.StreamReader($webResponse.GetResponseStream())
         $stream.ReadToEnd() | Set-Content -Path $FilePath -Force
     } catch [System.Net.WebException] {
         # If content isn't modified according to the output file timestamp then ignore the exception

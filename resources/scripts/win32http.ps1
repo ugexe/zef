222c626efd764fc64e97ac3db1860216520ab391
Nick Logan <nlogan@gmail.com>
2017-11-16 11:37:46 -0500

    win: remove problematic fetching logic

--- resources/scripts/win32http.ps1
+++ resources/scripts/win32http.ps1
@@ -4,24 +4,6 @@ Param (
     $UserAgent = "rakudo perl6/zef powershell downloader"
 )
 
-if ( -not (Test-Path $FilePath) ) {
-    $client = New-Object System.Net.WebClient;
-    $client.Headers['User-Agent'] = $UserAgent;
-    $client.DownloadFile($uri.ToString(), $FilePath)
-} else {
-    try {
-        $webRequest = [System.Net.HttpWebRequest]::Create($uri);
-        $webRequest.IfModifiedSince = ([System.IO.FileInfo]$FilePath).LastWriteTime
-        $webRequest.UserAgent = $UserAgent;
-        $webRequest.Method = "GET";
-        [System.Net.HttpWebResponse]$webResponse = $webRequest.GetResponse()
-
-        $stream = New-Object System.IO.StreamReader($webResponse.GetResponseStream())
-        $stream.ReadToEnd() | Set-Content -Path $FilePath -Force
-    } catch [System.Net.WebException] {
-        # If content isn't modified according to the output file timestamp then ignore the exception
-        if ($_.Exception.Response.StatusCode -ne [System.Net.HttpStatusCode]::NotModified) {
-            throw $_
-        }
-    }
-}
+$client = New-Object System.Net.WebClient;
+$client.Headers['User-Agent'] = $UserAgent;
+$client.DownloadFile($uri.ToString(), $FilePath)
